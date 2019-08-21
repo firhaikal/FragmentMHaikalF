247d45cd63e2cff6f997370edb2f1742a86d0ba4
Haikal Firdaus <haikal.firdaus.m@gmail.com>
2019-08-21 09:20:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment/Fragment_dashboard.java
@@ -0,0 +1,30 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Fragment_dashboard.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Fragment_dashboard#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Fragment_dashboard extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fragment_dashboard, null);
+    }
+}
---
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.fragment;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new Fragment_home());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment = new Fragment_home();
+                break;
+            case R.id.navigation_dashboard:
+                fragment = new Fragment_dashboard();
+                break;
+            case R.id.navigation_notifications:
+                fragment = new Fragment_notification();
+                break;
+        }
+        return loadFragment(fragment);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
